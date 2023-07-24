a170b0659305fd574ac9586fb540ae27819ad520
Massimo Colella <massimo-colella@outlook.it>
2023-07-25 01:46:52 +0200

    aggiustato qualche bug

--- src/components/SendMessageComp.vue
+++ src/components/SendMessageComp.vue
@@ -1,19 +1,19 @@
 <template>
-    <form>
+    <form @submit.prevent="getData()">
         <h2>Invia un messaggio al professore!</h2>
         <div class="mb-3">
             <label for="name" class="form-label">Nome e cognome</label>
-            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" v-model="name">
+            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" v-model="params.name">
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Indirizzo email</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" v-model="email">
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" v-model="params.email">
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="message" rows="3" name="message" v-model="message"></textarea>
+            <textarea class="form-control" id="message" rows="3" name="message" v-model="params.message"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary" @click="getData()">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </template>
 <script>
@@ -24,20 +24,18 @@ export default {
     props: ['idTeacher'],
     data() {
         return {
-                name: '',
-                email: '',
-                message: '',
+            'params':{
+                'teacher_id': this.idTeacher,
+                'name': '',
+                'email': '',
+                'message': '',
+            }
         }
     },
     methods: {
         getData(){
             
-            axios.get('http://127.0.0.1:8000/api/messages', {params:{
-                teacher_id: this.idTeacher,
-                name: this.name,
-                email: this.email,
-                message: this.message,
-            }})
+            axios.post('http://127.0.0.1:8000/api/teachers/message', this.params)
             .then(res => {
                 console.log(res.data)
             })
