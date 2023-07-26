b7569347f6e7aa96ada7c732a6b62cb1f07f8cb6
gluke0 <milanesigianluca@gmail.com>
2023-07-26 16:09:17 +0200

    fixed cv

--- src/components/views/SingleTeacher.vue
+++ src/components/views/SingleTeacher.vue
@@ -22,7 +22,7 @@
                 <p class="card-text"> Città: {{ elem.city }}</p>
                 <p class="card-text"> CAP: {{ elem.cap }}</p>
                 <p class="card-text"> Indirizzo: {{ elem.address }}</p>
-                <a v-if="elem.cv !== null" class="text-decoration-none" :href="elem.cv">Guarda Curriculum Vitae</a>
+                <a v-if="elem.cv !== null" class="text-decoration-none" target="_blank" :href="`http://127.0.0.1:8000/storage/${elem.cv}`">Guarda Curriculum Vitae</a>
                 <p class="card-text" v-if="elem.cv === null"> Nessun Curriculum Vitae disponibile</p>
                 <p class="card-text"> Tariffa oraria: {{ elem.price }} € </p>
                 <p class="card-text" v-if="elem.remote === 1"> Disponibile per lezioni in remoto</p>
