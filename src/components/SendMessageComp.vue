36130d9e1ac0167a2a302299ee67fe290484f94b
gluke0 <milanesigianluca@gmail.com>
2023-07-26 16:57:30 +0200

    client side validation

--- src/components/SendMessageComp.vue
+++ src/components/SendMessageComp.vue
@@ -3,15 +3,15 @@
         <h2>Chiedi informazioni:</h2>
         <div class="mb-3">
             <label for="name" class="form-label">Nome*</label>
-            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" v-model="params.name">
+            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" v-model="params.name" required>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">E-mail*</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" v-model="params.email">
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" v-model="params.email" required>
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">Descrizione:*</label>
-            <textarea class="form-control" id="message" rows="3" name="message" v-model="params.message"></textarea>
+            <textarea class="form-control" id="message" rows="3" name="message" v-model="params.message" required></textarea>
         </div>
         <span for="name" class="col-md-6 text-md-right campi ">I campi contrassegnati da * sono obblgatori.</span>
         <br>
@@ -23,15 +23,15 @@
         <h2>Lascia una recensione: </h2>
         <div class="mb-3">
             <label for="name" class="form-label">Nome*</label>
-            <input type="text" class="form-control" id="name-1" aria-describedby="emailHelp" v-model="review.name">
+            <input type="text" class="form-control" id="name-1" aria-describedby="emailHelp" v-model="review.name" required>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">E-mail*</label>
-            <input type="email" class="form-control" id="email-1" aria-describedby="emailHelp" v-model="review.email">
+            <input type="email" class="form-control" id="email-1" aria-describedby="emailHelp" v-model="review.email" required>
         </div>
         <div class="mb-3">
             <label for="rate" class="form-label">Voto*</label>
-            <input type="number" class="form-control" id="rate-1" aria-describedby="emailHelp" v-model="review.rate" min="0" max="5">
+            <input type="number" class="form-control" id="rate-1" aria-describedby="emailHelp" v-model="review.rate" min="1" max="5" required>
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">Recensione*</label>
@@ -50,11 +50,12 @@ export default {
     props: ['idTeacher'],
     data() {
         return {
+            errors: [],
             'params':{
                 'teacher_id': this.idTeacher,
-                'name': '',
-                'email': '',
-                'message': '',
+                name: null,
+                email: null,
+                message: null,
             },
             'review':{
                 'teacher_id': this.idTeacher,
@@ -86,7 +87,17 @@ export default {
                     console.error(error);
             });
         },
-    },   
+        checkForm: function (e){
+            if (this.params.name){
+                return true;
+            }
+            this.errors = [];
+            if (!this.params.name){
+                this.errors.push('Nome obbligatorio');
+            }
+            e.preventDefault();
+        }   
+     },   
 }
 </script>
 <style lang="scss">
