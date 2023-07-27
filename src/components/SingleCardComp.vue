f685f67dd6af001514117457729a2fb6c5acd4af
gluke0 <milanesigianluca@gmail.com>
2023-07-27 12:24:54 +0200

    img are back

--- src/components/SingleCardComp.vue
+++ src/components/SingleCardComp.vue
@@ -2,7 +2,7 @@
     <div class="col col-md-4 my-3">
         <router-link  :to="{name: 'single-teacher', params: { id: detailsTeachers.id }}">
             <div class="card">
-                <img v-if="detailsTeachers.profile_picture !== null" :src="detailsTeachers.profile_picture" class="card-img-top" alt="">
+                <img v-if="detailsTeachers.profile_picture !== null" :src="`http://127.0.0.1:8000/storage/${detailsTeachers.profile_picture}`" class="card-img-top" alt="">
                 <img v-if="detailsTeachers.profile_picture === null" src="../assets/img/pp.jpg" class="card-img-top" alt="default profile picture">
                 <div class="card-body">
                     <h5 class="card-title">{{ detailsTeachers.user.name }} {{ detailsTeachers.user.surname }}</h5>
